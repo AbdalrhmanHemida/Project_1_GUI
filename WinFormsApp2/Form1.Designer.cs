177707eda3ef9735bde1e3e83f3b863f77156eaf
Abdalrhman Mohamed Hemida <96482182+AbdalrhmanHemida@users.noreply.github.com>
2023-10-25 00:50:59 +0300

    video 1

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(35, 78);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(109, 32);
+            button1.TabIndex = 0;
+            button1.Text = "Refresh";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(188, 78);
+            button2.Margin = new Padding(2);
+            button2.Name = "button2";
+            button2.Size = new Size(109, 32);
+            button2.TabIndex = 1;
+            button2.Text = "Connect";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(348, 78);
+            button3.Margin = new Padding(2);
+            button3.Name = "button3";
+            button3.Size = new Size(109, 32);
+            button3.TabIndex = 2;
+            button3.Text = "Disconnect";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(35, 31);
+            comboBox1.Margin = new Padding(2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(207, 23);
+            comboBox1.TabIndex = 3;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(256, 36);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Disconnected";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(35, 153);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 23);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(139, 206);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 6;
+            button4.Text = "send";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(560, 270);
+            Controls.Add(button4);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ComboBox comboBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button4;
     }
 }
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -1,10 +1,132 @@
+using System.IO.Ports;
+
 namespace WinFormsApp2
 {
     public partial class Form1 : Form
     {
+        SerialPort port = null;
         public Form1()
         {
             InitializeComponent();
+            refresh_com();
+            label1.Text = "ddis connected";
+            label1.ForeColor = Color.Red;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            refresh_com();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            connect();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            disconnect();
+        }
+
+        private void refresh_com()
+        {
+            try
+            {
+                string[] portNames = SerialPort.GetPortNames();
+
+                if (portNames.Length > 0)
+                {
+                    comboBox1.DataSource = portNames;
+                }
+                else
+                {
+                    comboBox1.DataSource = null;
+                    comboBox1.Items.Add("No ports available");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions or log them
+                MessageBox.Show("Error: refresh_com() method" + ex.Message);
+            }
+        }
+
+        private void connect()
+        {
+            port = new SerialPort(comboBox1.SelectedItem.ToString());
+            port.BaudRate = 9600;
+            port.DataBits = 8;
+            port.StopBits = StopBits.One;
+
+            try
+            {
+                if (!port.IsOpen)
+                {
+                    port.Open();
+                    label1.Text = "connected";
+                    label1.ForeColor = Color.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: connect() method" + ex.Message);
+            }
+        }
+
+        private void disconnect()
+        {
+            try
+            {
+                if (port.IsOpen)
+                {
+                    port.Close();
+                    label1.Text = "ddis connected";
+                    label1.ForeColor = Color.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: disconnect() method" + ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            disconnect();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            send();
+        }
+
+        private void send()
+        {
+            try
+            {
+                port.Write(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: send() method" + ex.Message);
+
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
